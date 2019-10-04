5a7f980c63e2257f978ce667cf1db4a13b0e50cb
Fikri Arifin <fikriarifinn.fa@gmail.com>
2019-10-04 14:23:33 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,8 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsam maiores ea repellat adipisci doloremque dolorem, labore, ab laudantium facere esse culpa excepturi debitis nihil molestiae iste deserunt voluptates sint?</p>
+    <ul>
+        <link rel="alternate" href="atom.xml" type="application/atom+xml" title="Atom">
+    </ul>
 </body>
 </html>
